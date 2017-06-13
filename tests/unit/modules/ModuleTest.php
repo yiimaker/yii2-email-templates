b6293fd4bfebf8189c98a6667f3bcb3999654557
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-06-13 15:14:37 +0000

    Creates tests for module

---
+++ tests/unit/modules/ModuleTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @link https://github.com/yiimaker/yii2-email-templates
+ * @copyright Copyright (c) 2017 Yii Maker
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\email\templates\tests\unit\modules;
+
+use Yii;
+use ymaker\email\templates\tests\unit\TestCase;
+use ymaker\email\templates\Module;
+use ymaker\email\templates\services\DbService;
+use ymaker\email\templates\services\ServiceInterface;
+use yii2deman\tools\i18n\LanguageProviderInterface;
+
+/**
+ * Test case for module
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @since 1.0
+ */
+class ModuleTest extends TestCase
+{
+    public function testInitService()
+    {
+        $module = new Module('test', null, ['languageProvider' => []]);
+        $expected = ['class' => DbService::class];
+
+        $this->assertEquals($expected, $module->service);
+    }
+
+    public function testInitLanguageProvider()
+    {
+        $this->expectException('yii\base\InvalidConfigException');
+        $module = new Module('test');
+    }
+
+    public function testInit()
+    {
+        $module = new Module('test', null, ['languageProvider' => []]);
+
+        $this->assertTrue(Yii::$container->has(ServiceInterface::class));
+        $this->assertTrue(Yii::$container->has(LanguageProviderInterface::class));
+    }
+}

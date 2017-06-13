9b37e6e47745ca01a08a8ae64b257bcdf8d0a6ae
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-06-13 11:03:35 +0000

    Fixes configuration of codeception

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,7 +4,6 @@ error_reporting(-1);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
 defined('YII_ENV') or define('YII_ENV', 'test');
-//defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . implode(DIRECTORY_SEPARATOR, ['', '..' ,'..', '..']));
 defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . implode(DIRECTORY_SEPARATOR, ['', '..', 'vendor']));
 
 require_once(VENDOR_DIR . DIRECTORY_SEPARATOR . 'autoload.php');

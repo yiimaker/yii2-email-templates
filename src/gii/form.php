5b8be993f036b3caa8989c7c8a53700b4e4ffb8a
Geoffry Van den Eede <geoffry304@live.be>
2018-05-24 13:04:00 +0200

    Gii generator: adds default value to the hint field (#24)

--- src/gii/form.php
+++ src/gii/form.php
@@ -13,5 +13,5 @@
 echo $form->field($generator, 'key')->textInput(['autofocus' => true]);
 echo $form->field($generator, 'subject');
 echo $form->field($generator, 'body')->textarea();
-echo $form->field($generator, 'hint');
+echo $form->field($generator, 'hint')->textInput(['value' => 'All tokens wrapped in {} will be replaced by real data']);
 echo $form->field($generator, 'migrationName')->hiddenInput();
